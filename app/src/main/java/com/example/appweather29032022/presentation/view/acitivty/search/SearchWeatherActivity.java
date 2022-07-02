21517a033ed1829afa2d86bc220bbb187eec7d5b
Phat <phatdroid94@gmail.com>
2022-07-02 18:45:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/appweather29032022/presentation/view/acitivty/search/SearchWeatherActivity.java
@@ -0,0 +1,16 @@
+package com.example.appweather29032022.presentation.view.acitivty.search;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appweather29032022.R;
+
+public class SearchWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_weather);
+    }
+}
