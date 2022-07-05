58175b3d66c1693bc9eb34299e2c292dfe775867
Phat <phatdroid94@gmail.com>
2022-07-05 18:08:00 +0700

    weather search response

---
+++ app/src/main/java/com/example/appweather29032022/data/remote/model/WeatherSearchResponse.java
@@ -0,0 +1,205 @@
+package com.example.appweather29032022.data.remote.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by pphat on 7/5/2022.
+ */
+public class WeatherSearchResponse {
+    private List<WeatherDTO> weather;
+    private MainDTO main;
+    private WindDTO wind;
+    private int dt;
+    private SysDTO sys;
+    private String name;
+
+    public List<WeatherDTO> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(List<WeatherDTO> weather) {
+        this.weather = weather;
+    }
+
+    public MainDTO getMain() {
+        return main;
+    }
+
+    public void setMain(MainDTO main) {
+        this.main = main;
+    }
+
+    public WindDTO getWind() {
+        return wind;
+    }
+
+    public void setWind(WindDTO wind) {
+        this.wind = wind;
+    }
+
+    public int getDt() {
+        return dt;
+    }
+
+    public void setDt(int dt) {
+        this.dt = dt;
+    }
+
+    public SysDTO getSys() {
+        return sys;
+    }
+
+    public void setSys(SysDTO sys) {
+        this.sys = sys;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public static class CoordDTO {
+        @SerializedName("lon")
+        private double lon;
+        @SerializedName("lat")
+        private double lat;
+
+        public double getLon() {
+            return lon;
+        }
+
+        public void setLon(double lon) {
+            this.lon = lon;
+        }
+
+        public double getLat() {
+            return lat;
+        }
+
+        public void setLat(double lat) {
+            this.lat = lat;
+        }
+    }
+
+    public static class MainDTO {
+        @SerializedName("temp")
+        private int temp;
+        @SerializedName("temp_min")
+        private int tempMin;
+        @SerializedName("temp_max")
+        private int tempMax;
+        @SerializedName("pressure")
+        private int pressure;
+        @SerializedName("humidity")
+        private int humidity;
+
+        public int getTemp() {
+            return temp;
+        }
+
+        public void setTemp(int temp) {
+            this.temp = temp;
+        }
+
+        public int getTempMin() {
+            return tempMin;
+        }
+
+        public void setTempMin(int tempMin) {
+            this.tempMin = tempMin;
+        }
+
+        public int getTempMax() {
+            return tempMax;
+        }
+
+        public void setTempMax(int tempMax) {
+            this.tempMax = tempMax;
+        }
+
+        public int getPressure() {
+            return pressure;
+        }
+
+        public void setPressure(int pressure) {
+            this.pressure = pressure;
+        }
+
+        public int getHumidity() {
+            return humidity;
+        }
+
+        public void setHumidity(int humidity) {
+            this.humidity = humidity;
+        }
+    }
+
+    public static class WindDTO {
+        private double speed;
+
+        public double getSpeed() {
+            return speed;
+        }
+
+        public void setSpeed(double speed) {
+            this.speed = speed;
+        }
+    }
+
+    public static class SysDTO {
+        private String country;
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+    }
+
+    public static class WeatherDTO {
+        private int id;
+        private String main;
+        private String description;
+        private String icon;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getMain() {
+            return main;
+        }
+
+        public void setMain(String main) {
+            this.main = main;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
+
+        public void setIcon(String icon) {
+            this.icon = icon;
+        }
+    }
+}
